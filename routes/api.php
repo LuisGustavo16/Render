55868c787737d004771ee8824cae66e50a34c61d
LuisGustavo16 <guluisgustavo300@gmail.com>
2024-09-19 20:19:00 -0300

    adicionando a api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TreinoAmistosoController;
+use App\Http\Controllers\API\ModalidadeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/*Controller dos Treinos e Amistosos*/
+Route::get ('/treino_amistosos/listarTreinos', [TreinoAmistosoController::class, 'index']); // Rota para exibir
+Route::post('/treino_amistosos/cadastrarTreino', [TreinoAmistosoController::class, 'store']); // Rota para cadastrar
+
+Route::get ('/modalidades/ListarModalidades', [ModalidadeController::class, 'index']); // Rota para exibir
+Route::post('/modalidades/cadastrarModalidade', [ModalidadeController::class, 'store']); // Rota para cadastrar
